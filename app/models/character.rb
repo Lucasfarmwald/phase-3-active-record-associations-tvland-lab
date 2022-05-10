59fca8df5048d40ff84d6c9a890e0a2ce31a0832
lucasfarmwald <farmwaldlucas@gmail.com>
2022-05-09 20:29:18 -0500

    commit tree 83bad53d6a0a7305acec15275fcc07553299d5f8

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,8 @@
 class Character < ActiveRecord::Base
-   def actor 
-    self.actors
-   end
-   def show 
-    self.shows
-   end
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+   "#{self.name} always says: #{self.catchphrase}"
+  end
 end
